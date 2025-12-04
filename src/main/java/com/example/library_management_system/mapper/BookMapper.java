856bc13b3a7a6e6c675d884b234f03852ebdc625
Ervin Mamutov <ervin.news@gmail.com>
2025-12-04 08:05:10 +0100

    modify BookMapper

--- src/main/java/com/example/library_management_system/mapper/BookMapper.java
+++ src/main/java/com/example/library_management_system/mapper/BookMapper.java
@@ -1,7 +1,9 @@
 package com.example.library_management_system.mapper;
 
 import com.example.library_management_system.dto.book.BookCreateRequestDTO;
+import com.example.library_management_system.dto.book.BookImportDTO;
 import com.example.library_management_system.dto.book.BookResponseDTO;
+import com.example.library_management_system.dto.book.BookUpdateRequestDTO;
 import com.example.library_management_system.model.Book;
 import org.springframework.stereotype.Component;
 
@@ -17,6 +19,25 @@ public class BookMapper {
             dto.getPublicationYear(),
             dto.getCopiesAvailable()
     );
+  }
+
+  public Book toEntity(BookImportDTO dto) {
+    return new Book(
+            dto.getTitle(),
+            dto.getAuthor(),
+            dto.getIsbn(),
+            dto.getGenre(),
+            dto.getPublicationYear(),
+            dto.getCopies()
+    );
+  }
+
+  public void updateEntity(Book book, BookUpdateRequestDTO dto) {
+    book.setTitle(dto.getTitle());
+    book.setAuthor(dto.getAuthor());
+    book.setGenre(dto.getGenre());
+    book.setPublicationYear(dto.getPublicationYear());
+    book.setCopiesAvailable(dto.getCopiesAvailable());
   }
 
   public BookResponseDTO toResponseDTO(Book book) {
